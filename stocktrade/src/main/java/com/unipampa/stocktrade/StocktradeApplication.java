2082792d0d0f85918379c2830c9abb77a63072fd
FCardozera <felipebcardoso0711@outlook.com>
2023-09-05 19:44:26 -0300

    build: :building_construction: Configuração inicial do ambiente SpringBoot
    Foi configurado o ambiente SpringBoot com jdk20, separando as camadas da arquitetura e adicionando as dependências iniciais necessárias
    
    Issue #4 : Configurar ambiente Springboot 05/09

---
+++ stocktrade/src/main/java/com/unipampa/stocktrade/StocktradeApplication.java
@@ -0,0 +1,13 @@
+package com.unipampa.stocktrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktradeApplication.class, args);
+	}
+
+}
---
+++ stocktrade/src/test/java/com/unipampa/stocktrade/StocktradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipampa.stocktrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
