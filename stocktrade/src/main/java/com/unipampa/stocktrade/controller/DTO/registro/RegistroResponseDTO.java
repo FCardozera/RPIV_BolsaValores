dca6500102b346d7399068871ed665bb0fd89b29
Ricardo Costa <ricardo_hentges@hotmail.com>
2023-09-22 17:53:06 -0300

    style: :art: Formatação de código
    O código foi formatado para deixar tudo em uma só linha

--- stocktrade/src/main/java/com/unipampa/stocktrade/controller/DTO/registro/RegistroResponseDTO.java
+++ stocktrade/src/main/java/com/unipampa/stocktrade/controller/DTO/registro/RegistroResponseDTO.java
@@ -4,12 +4,8 @@ import java.time.Instant;
 import java.util.UUID;
 import com.unipampa.stocktrade.model.entity.registro.Registro;
 
-public record RegistroResponseDTO(
-    UUID id,
-    UUID usuarioId,
-    String atividade,
-    Instant instant
-) { 
+public record RegistroResponseDTO(UUID id, UUID usuarioId, String atividade, Instant instant) { 
+    
     public RegistroResponseDTO (Registro registro){
         this(registro.getId(), registro.getUsuarioId(), registro.getAtividade(), registro.getInstant());
     }
