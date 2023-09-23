48ffecfb80bac11ea7a1295b61a198fb18cc2c91
Joao Medina <joaomedinap@gmail.com>
2023-09-23 15:21:01 -0300

    style: :art: indexLogado e  carteira interface
    Adesão tela indexLogado e inicio da carteira

---
+++ stocktrade/src/main/java/com/unipampa/stocktrade/controller/ControllerCarteira.java
@@ -0,0 +1,19 @@
+package com.unipampa.stocktrade.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/carteira")
+public class ControllerCarteira {
+    
+    @GetMapping
+    public ModelAndView carteiraPagina() {
+        ModelAndView mv = new ModelAndView("/carteira");
+        return mv;
+    }
+
+}
