0169e74aaa54448638cf6584ff0888533e22b0b6
Ricardo Costa <ricardo_hentges@hotmail.com>
2023-11-12 17:48:56 -0300

    fix: :bug: Bug no repositório do registro
    O identificador do Registro estava como Integer ao invés de ser UUID

--- stocktrade/src/main/java/com/unipampa/stocktrade/model/repository/registro/RegistroRepository.java
+++ stocktrade/src/main/java/com/unipampa/stocktrade/model/repository/registro/RegistroRepository.java
@@ -1,8 +1,10 @@
 package com.unipampa.stocktrade.model.repository.registro;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.unipampa.stocktrade.model.entity.registro.Registro;
 
-public interface RegistroRepository extends JpaRepository<Registro, Integer> {
+public interface RegistroRepository extends JpaRepository<Registro, UUID> {
     
 }
