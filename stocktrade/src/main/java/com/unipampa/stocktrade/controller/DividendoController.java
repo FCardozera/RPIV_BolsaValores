c52d2a036c923c8d0ccd49f127f95bee37d19748
Tales Soares <talesschifelbeinsoares@gmail.com>
2023-11-21 11:02:13 -0300

    feat: :sparkles: Adicionado a tela de gestão de dividendos

---
+++ stocktrade/src/main/java/com/unipampa/stocktrade/controller/DividendoController.java
@@ -0,0 +1,17 @@
+package com.unipampa.stocktrade.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/dividendo")
+public class DividendoController {
+    
+    @GetMapping
+    public ModelAndView dividendoPagina() {
+        return new ModelAndView("/dividendo");
+    }
+}
