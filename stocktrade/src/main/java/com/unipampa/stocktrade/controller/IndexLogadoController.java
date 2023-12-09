c27442559a8a296b4fb6ab7190fb8159e48058ad
Tales Soares <talesschifelbeinsoares@gmail.com>
2023-12-09 18:42:00 -0300

    feat: :art: Adição do carrosel no invistaLogado, adição do alert e  correções de front
    Adicionado alert para confirmação de logout do sistema.  Correção de detalhes no front, tamanho de botões, tamanho de fonte, cores.  Adição do carrosel na página invistaLogado.

--- stocktrade/src/main/java/com/unipampa/stocktrade/controller/IndexLogadoController.java
+++ stocktrade/src/main/java/com/unipampa/stocktrade/controller/IndexLogadoController.java
@@ -1,17 +1,33 @@
 package com.unipampa.stocktrade.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.unipampa.stocktrade.service.IndexLogadoService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class IndexLogadoController {
-    
+
+
+    @Autowired
+    private IndexLogadoService indexLogadoService;
+
     @GetMapping("/indexLogado")
-    public ModelAndView indexLogado (HttpSession session) {
-        return new ModelAndView("indexLogado");
+    public ModelAndView indexLogado(HttpSession session) {
+
+        ModelAndView mv = new ModelAndView("/indexLogado");
+
+        List<String[]> acoesUsuario = indexLogadoService.getAcoesUser(session);
+        mv.addObject("acoesUsuario", acoesUsuario);
+
+        return mv;
+
     }
 
 }
